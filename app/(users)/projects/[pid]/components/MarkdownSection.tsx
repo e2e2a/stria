ed8f56654208f1f9ea1f5e8ca9e3f0b92d0a0c0a
Reymond Godoy <emonawong22@gmail.com>
2026-04-14 11:37:22 +0800

    transfer code and make a file editor-tab-title-bar.tsx for the options feature

--- app/(users)/projects/[pid]/components/MarkdownSection.tsx
+++ app/(users)/projects/[pid]/components/MarkdownSection.tsx
@@ -34,7 +34,6 @@ import { HocuspocusProvider } from '@hocuspocus/provider';
 import * as Y from 'yjs';
 import { useTabStore } from '@/features/editor/stores/tabs';
 import { useParams } from 'next/navigation';
-import { EditorOptions } from './editor-options';
 import { useNodeStore } from '@/features/editor/stores/nodes';
 import { useProjectUIStore } from '@/features/editor/stores/project-ui';
 import ContextMenuClient from './context-menu/context-menu-client';
@@ -45,6 +44,7 @@ import { useEditorEvents } from './use-editor-events';
 import { ChunkEditor } from './chunk-editor';
 import { cn } from '@/lib/utils';
 import { useQueryClient } from '@tanstack/react-query';
+import EditorTabTitleBar from './options/appearance/editor-tab-title-bar';
 
 const myOwnDarkTheme = createTheme({
   theme: 'dark',
@@ -307,22 +307,15 @@ function MarkdownSection({ node, isDirty, canEditNode, canEditChunk }: { node: I
 
   return (
     <>
-      <div className="absolute top-12 left-0 right-0 h-1 z-51 w-full bg-background" />
-      <div className="absolute top-13 left-0 right-0 h-14 z-50 flex items-center px-10 border-b border-white/5 bg-sidebar/80 backdrop-blur-sm pointer-events-auto cursor-default drop-shadow-xs shadow-xs">
-        <div className="flex justify-between items-center w-full">
-          <h1 className="text-5xl font-bold tracking-tighter text-foreground truncate select-text w-fit! cursor-text">
-            {(node as INode)?.title || 'Not Found'}
-          </h1>
-          <EditorOptions
-            editorViewRef={editorViewRef}
-            isReadOnly={isReadOnly}
-            setIsReadOnly={setIsReadOnly}
-            isChunkActive={isChunkActive}
-            setIsChunkActive={setIsChunkActive}
-            canEditChunk={canEditChunk}
-          />
-        </div>
-      </div>
+      <EditorTabTitleBar
+        node={node}
+        editorViewRef={editorViewRef}
+        isReadOnly={isReadOnly}
+        setIsReadOnly={setIsReadOnly}
+        isChunkActive={isChunkActive}
+        setIsChunkActive={setIsChunkActive}
+        canEditChunk={canEditChunk}
+      />
 
       {isChunkActive && (
         <div
---
+++ app/(users)/projects/[pid]/components/options/appearance/editor-tab-title-bar.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { EditorOptions } from '../../editor-options';
+import { EditorView } from '@uiw/react-codemirror';
+import { INode } from '@/types';
+
+const EditorTabTitleBar = ({
+  node,
+  editorViewRef,
+  isReadOnly,
+  setIsReadOnly,
+  isChunkActive,
+  setIsChunkActive,
+  canEditChunk,
+}: {
+  node: INode;
+  editorViewRef: React.RefObject<EditorView | null>;
+  isReadOnly: boolean;
+  setIsReadOnly: (val: boolean | ((prev: boolean) => boolean)) => void;
+  isChunkActive: boolean;
+  setIsChunkActive: (val: boolean | ((prev: boolean) => boolean)) => void;
+  canEditChunk: boolean;
+}) => {
+  return (
+    <>
+      <div className="absolute top-12 left-0 right-0 h-1 z-51 w-full bg-background" />
+      <div className="absolute top-13 left-0 right-0 h-14 z-50 flex items-center px-10 border-b border-white/5 bg-sidebar/80 backdrop-blur-sm pointer-events-auto cursor-default drop-shadow-xs shadow-xs">
+        <div className="flex justify-between items-center w-full">
+          <h1 className="text-5xl font-bold tracking-tighter text-foreground truncate select-text w-fit! cursor-text">
+            {(node as INode)?.title || 'Not Found'}
+          </h1>
+          <EditorOptions
+            editorViewRef={editorViewRef}
+            isReadOnly={isReadOnly}
+            setIsReadOnly={setIsReadOnly}
+            isChunkActive={isChunkActive}
+            setIsChunkActive={setIsChunkActive}
+            canEditChunk={canEditChunk}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default EditorTabTitleBar;
