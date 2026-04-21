95b51ff20d185a79cf8e3dfce473023020bb6fe1
Reymond Godoy <emonawong22@gmail.com>
2026-04-21 12:30:17 +0800

    add

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
   devIndicators: false,
-  experimental: {
-    optimizePackageImports: ['lucide-react', '@radix-ui/react-*', '@tanstack/react-table', '@tanstack/react-query', 'recharts'],
-  },
-  allowedDevOrigins: ['responsibilities-protest-realistic-listening.trycloudflare.com'],
+  // experimental: {
+  //   optimizePackageImports: ['lucide-react', '@radix-ui/react-icons', '@tanstack/react-table', '@tanstack/react-query', 'recharts'],
+  // },
+  allowedDevOrigins: ['localhost:3000', 'responsibilities-protest-realistic-listening.trycloudflare.com'],
   transpilePackages: ['yjs', '@hocuspocus/server', '@hocuspocus/transformer'],
   poweredByHeader: false,
 };
